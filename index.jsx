8548bfe7fec97b44e967af757877e0a3054f267b
bbrazis <bradley.brazis@gmail.com>
2024-06-27 11:28:28 -0700

    Reorganized and added host

--- index.jsx
+++ index.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/Vans'
-import VanDetail from './pages/VanDetail'
-import Layout from './components/Layout';
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Layout from './components/Layout'
+import HostLayout from './components/HostLayout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 
 
 import './server'
@@ -19,6 +23,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
