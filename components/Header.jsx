52d75717f6d4819dd5075b2c81bd9630154ea7f6
bbrazis <bradley.brazis@gmail.com>
2024-07-01 10:22:14 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header(){
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink to="/host" className={({isActive}) => isActive ? 'link-active': null}>Host</NavLink>
-                <NavLink to="/about" className={({isActive}) => isActive ? 'link-active': null}>About</NavLink>
-                <NavLink to="/vans" className={({isActive}) => isActive ? 'link-active': null}>Vans</NavLink>
+                <NavLink to="host" className={({isActive}) => isActive ? 'link-active': null}>Host</NavLink>
+                <NavLink to="about" className={({isActive}) => isActive ? 'link-active': null}>About</NavLink>
+                <NavLink to="vans" className={({isActive}) => isActive ? 'link-active': null}>Vans</NavLink>
             </nav>
         </header>
     )
