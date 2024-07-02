06eb88654a5ef18248381727564a53e69074a9e9
bbrazis <bradley.brazis@gmail.com>
2024-07-02 11:19:17 -0700

    Update Vans.jsx

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 export default function Vans() {
-    
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
     
+    const typeFilter = searchParams.get("type")
+
     React.useEffect(()=>{
         fetch('/api/vans')
             .then(res => res.json())
             .then(data => setVans(data.vans))
     },[])
+
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
     
     const Van = ({imageUrl, name, price, type, id}) =>
         <div key={id} className="van-tile">
@@ -29,8 +35,14 @@ export default function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <button className="van-type simple" onClick={() => searchFor("type","simple")}>Simple</button>
+                <button className="van-type luxury" onClick={() => searchFor("type","luxury")}>Luxury</button>
+                <button className="van-type rugged" onClick={() => searchFor("type","rugged")}>Rugged</button>
+                <button className="van-type clear-filters" onClick={() => clearSearch()}>Clear filter</button>
+            </div>
             <div className="van-list">
-                {vans.length > 0 && vans.map(Van)}
+                {vans.length > 0 && displayedVans.map(Van)}
             </div>
         </div>
     )
