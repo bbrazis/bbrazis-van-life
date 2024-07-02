0d9616ed8aaf4b8d8fd59b72b720209b7cc2d732
bbrazis <bradley.brazis@gmail.com>
2024-07-02 13:42:42 -0700

    Update Vans.jsx

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -19,7 +19,7 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            <Link to={`${van.id}`}>
                 <img src={van.imageUrl} alt={`${van.name} camped out in the wilderness`}/>
                 <div className="van-info">
                     <h3>{van.name}</h3>
