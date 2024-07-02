5d8de4554aa812ab736a6f0f85c2bad1e7056024
bbrazis <bradley.brazis@gmail.com>
2024-07-02 09:50:03 -0700

    Update HostVanPhotos.jsx

--- pages/Host/HostVanPhotos.jsx
+++ pages/Host/HostVanPhotos.jsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPhotos() {
     const { currentVan } = useOutletContext()
+    console.log(currentVan)
     return (
         <img src={currentVan.imageUrl} className="host-van-detail-image" alt={`a picutre of ${currentVan.name}`} />
     )
