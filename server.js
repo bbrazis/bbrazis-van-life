49964da4926585b9850635b0a7a4c184b1f03002
bbrazis <bradley.brazis@gmail.com>
2024-07-16 13:25:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ createServer({
         this.namespace = "api"
         this.logging = false
         // this.timing = 2000  // => mock a 2 second delay in server response
-        this.passthrough("https://x8ki-letl-twmt.n7.xano.io/api:-QKB4H52/**")
+        this.passthrough("https://x8ki-letl-twmt.n7.xano.io/**")
 
         this.get("/vans", (schema, request) => {
             // return new Response(400, {}, {error: "Error fetching data"})
