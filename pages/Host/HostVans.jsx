7976ce3be992060ffa96da421a8f9670791df116
bbrazis <bradley.brazis@gmail.com>
2024-07-18 11:41:39 -0700

    Update HostVans.jsx

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -4,13 +4,25 @@ import { getHostVans } from "../../api"
 
 export default function HostVans() {
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
     
     React.useEffect(() => {
-        getHostVans()
-            .then(data => setVans(data))
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getHostVans()
+                setVans(data)
+            } catch(err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
     }, [])
 
-    const hostVansEls = vans.map(van => (
+    const hostVansEls = vans?.map(van => (
         <Link
             to={`${van.id}`}
             key={van.id}
@@ -26,6 +38,13 @@ export default function HostVans() {
         </Link>
     ))
 
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
     return (
         <section>
             <h1 className="host-vans-title">Your listed vans</h1>
