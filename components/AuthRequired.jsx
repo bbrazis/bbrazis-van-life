2d35028e52afbd85598d7de39e7bd156f6789d60
bbrazis <bradley.brazis@gmail.com>
2024-07-17 20:38:04 -0700

    Update AuthRequired.jsx

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -1,23 +1,33 @@
 import React from "react"
-import { Outlet, Navigate, useLocation } from "react-router-dom"
+import { Outlet, Navigate, useLocation, redirect } from "react-router-dom"
 import { getCookie } from "../api"
 
 export default function AuthRequired() {
+    const [loaded, setLoaded] = React.useState(false)
     const isLoggedIn = getCookie('login')
     const location = useLocation()
     
-    if(!isLoggedIn) {
-        return (
-            <Navigate 
-                to="/login" 
-                state={{
-                    message: "You must log in first",
-                    from: location.pathname
-                }} 
-                replace
-            />
-        )
-    } else {
-        return <Outlet />
-    }
+    React.useEffect(()=>{
+        if(!loaded){
+            if(!isLoggedIn) {
+                return (
+                    <Navigate 
+                        to="/login" 
+                        state={{
+                            message: "You must log in first",
+                            from: location.pathname
+                        }} 
+                        replace
+                    />
+                )
+            } else {
+                return <Outlet />
+            }
+        }
+        setLoaded(true)
+    },[loaded])
+
+    return (
+        <div className="loader" aria-live="assertive"></div>
+    )
 }
