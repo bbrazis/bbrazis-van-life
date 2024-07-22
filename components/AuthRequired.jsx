c08e5173e388d2ee4b60c1c24a7536533cbf4152
bbrazis <bradley.brazis@gmail.com>
2024-07-22 15:12:48 -0700

    Update AuthRequired.jsx

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -32,7 +32,7 @@ export default function AuthRequired() {
             } catch(err) {
                 setError(err)
             } finally {
-                setloading(false)
+                //
             }
         }
 
